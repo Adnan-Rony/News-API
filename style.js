730dfd8b75ac0dc5d12455a317ad5727465de825
Adnan-Rony <adnanrony19@gmail.com>
2023-08-31 18:34:26 +0600

    half sesh

--- style.js
+++ style.js
@@ -22,7 +22,7 @@ const handleCtagory = async ()=>
 
         })
 
-     console.log(data.data.news_category);
+    //  console.log(data.data.news_category);
 
  
 
@@ -33,14 +33,87 @@ const handleLoadNews = async ( catagoryId)=>
 
         const data=await response.json();
 
-        console.log(data.data);
+        
+
+            const card=document.getElementById("card-container");
+            // 
+            card.innerHTML="";
+
+            data.data?.forEach((news)=>
+            
+           
+            {
+                console.log(news);
+                const div=document.createElement("div");
+               card.classList="grid gap-5 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 my-20"
+                div.innerHTML=`
+                <div class="card w-96 bg-base-100 shadow-xl">
+                <figure><img src="${news?.image_url}" alt="Shoes" /></figure>
+                <div class="card-body">
+                  <h2 class="card-title">${news.title.slice(0,40)}
+                  <button class="btn btn-secondary">${news.rating?.badge}</button>
+                  </h2>
+                 
+                  <p>${news.details.slice(0,80)}</p>
+                  <h3>total views:${news.total_view?news.total_view:"no views"}</h3>
+                 
+                </div>
+                <div class=" flex gap-5">
+                <img src="${news.author?.img}" alt="none" class="w-14 rounded-full " </img> 
+                <div>
+                <h6>${news.author?.name} </h6>
+                <h6>${news.author?.published_date} </h6>
+                </div>
+                <div >
+                <button onclick="handleModal("${news.news_id}")" class="btn btn-primary ">details</div>
+
+                </div>
+               
+
+              </div>
+                `
+                card.appendChild(div)
+
+            })
+         
+
+        
+            // console.log(data.data);
 
 
 
     };
+    const handleModal=async (newsId)=>{
 
+      const response=await fetch(`https://openapi.programming-hero.com/api/news/${newsId}`);
 
+      const data=await response.json();
+
+      console.log(data);
+
+      const modalcontainer=document.getElementById("modal-container")
+
+      const div=document.createElement('div')
+        div.innerHTML=`
+      
+      
+        <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+          <form method="dialog" class="modal-box">
+            <h3 class="font-bold text-lg">Hello!</h3>
+            <p class="py-4">Press ESC key or click the button below to close</p>
+            <div class="modal-action">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </div>
+          </form>
+        </dialog>
+        `  
+        modalcontainer.appendChild(div) ; 
+        const modal=document.getElementById("my_modal_5") ;
+        modal.showModal(); 
+    }
 
 
 
      handleCtagory();
+     handleLoadNews("01");
